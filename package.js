5bcc40055e24ddb33089514713e2183b79f0432b
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2015-09-02 17:40:11 +0300

    Implement Bitly OAuth

--- package.js
+++ package.js
@@ -10,8 +10,8 @@ var fs = Npm.require("fs");
 eval(fs.readFileSync("./packages/autopackage.js").toString());
 Package.onUse(function(api) {
   addFiles(api, description.name, getDefaultProfiles());
-  api.use(["foundation@1.0.0"]);
-  api.use(["core@1.0.0"]); // this will become a proper dependency once we separate the models like Recipes and Steps into their own repositories
+  api.use(["foundation"]);
+  api.use(["core"]); // this will become a proper dependency once we separate the models like Recipes and Steps into their own repositories
   api.export([
     "Pages"
   ]);
